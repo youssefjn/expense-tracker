d1eef8b6f8d1434e32194335c181703d74facefc
youssefjn <youssefjneyah@gmail.com>
2023-02-15 14:45:48 +0100

    exception

---
+++ src/main/java/expense/tracker/expensetracker/error/ExpenseNotFoundException.java
@@ -0,0 +1,10 @@
+package expense.tracker.expensetracker.error;
+
+public class ExpenseNotFoundException extends RuntimeException {
+
+    public ExpenseNotFoundException(String arg0) {
+        super(arg0);
+    }
+    
+    
+}
