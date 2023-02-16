7027b447e5cfdd719b3f8f7c56df3a5909022846
youssefjn <youssefjneyah@gmail.com>
2023-02-16 12:11:29 +0100

    fix validation

--- src/main/java/expense/tracker/expensetracker/dto/ExpenseDto.java
+++ src/main/java/expense/tracker/expensetracker/dto/ExpenseDto.java
@@ -5,6 +5,7 @@ import java.math.BigDecimal;
 import expense.tracker.expensetracker.model.ExpenseCategory;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -21,9 +22,9 @@ public class ExpenseDto {
     private Long id;
     @NotBlank
     private String expenseName;
-    @NotBlank
+    @NotNull
     private ExpenseCategory expenseCategory;
     @Min(value = 0)
-    @NotBlank
+    @NotNull
     private BigDecimal expenseAmount;
 }
